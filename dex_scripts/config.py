9abdf17ffd82feab7ba386be48ad0e0c3c1706c6
izrofid <izrofid@gmail.com>
2025-07-15 08:37:53 +0000

    feat: add dex scripts
    Added python scripts to automate data extraction

---
+++ dex_scripts/config.py
@@ -0,0 +1,57 @@
+OPTIONS = {
+    "species": {
+        "test": "Pokemon", 
+        "output": "speciesData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "abilities": {
+        "test": "Abilities", 
+        "output": "abilityData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "items": {
+        "test": "Items", 
+        "output": "itemData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "moves": {
+        "test": "Moves", 
+        "output": "moveData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "trainers": {
+        "test": "Trainers", 
+        "output": "trainerData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "types": {
+        "test": "Types", 
+        "output": "typeData", 
+        "is_pointer": False,
+        "post_processor": "none"
+        },
+
+    "sprites": {
+        "test": "Sprites", 
+        "output": "pokemonSprites", 
+        "is_pointer": True,
+        "post_processor": "sprite"
+        },
+
+    "trainersprites": {
+        "test": "Trainer Sprites", 
+        "output": "trainerSprites", 
+        "is_pointer": True,
+        "post_processor": "trainer_sprite"
+        },
+}
