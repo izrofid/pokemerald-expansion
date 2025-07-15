17b34df2d9baddad46234b16c13b1aaf314e0594
izrofid <izrofid@gmail.com>
2025-07-15 08:28:49 +0000

    chore: add gf_string function
    Added a function to convert GF charmap into UTF-8

---
+++ include/dex/gf_string.h
@@ -0,0 +1,12 @@
+#ifndef GF_STRING
+#define GF_STRING
+
+#include "global.h"
+
+// Converts one GF character to UTF-8. Writes to `out`, returns number of bytes.
+int ConvertGfCharToUtf8(u8 gfChar, u8 *out);
+
+// Converts a full GF-encoded string to a UTF-8 string.
+void ConvertGfStringToUtf8(const u8 *gfInput, char *utf8Output);
+
+#endif // GF_STRING
